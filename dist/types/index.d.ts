b9b6de7ffb7c64feb4d2ff34867922a16e13f28d
nicholaswagner <nicholas@secretsignal.com>
2025-03-21 12:02:47 -0500

    reworks the type generation and build scripts

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,8 @@
 import RemarkObsidious from "./RemarkObsidious";
 import { ObsidiousVault } from "./ObsidiousVault";
-export { ObsidiousVault, RemarkObsidious };
+import { hash, slugify, slugifyFilepath } from "./ObsidiousUtils";
+import { ObsidiousVaultImageFiletypes } from "./ObsidiousVault";
+export type { ObsidiousOptions, RemarkObsidiousOptions } from "./RemarkObsidious";
+export type { ObsidiousVaultItem, ObsidiousVaultData, ObsidiousFileTreeNode } from "./ObsidiousVault";
+export { ObsidiousVault, RemarkObsidious, hash, slugify, slugifyFilepath, ObsidiousVaultImageFiletypes };
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import RemarkObsidious from "./RemarkObsidious";
 import { ObsidiousVault } from "./ObsidiousVault";
+import { hash, slugify, slugifyFilepath } from "./ObsidiousUtils";
+import { ObsidiousVaultImageFiletypes } from "./ObsidiousVault";
 
-export { ObsidiousVault, RemarkObsidious };
+export type { ObsidiousOptions, RemarkObsidiousOptions } from "./RemarkObsidious";
+export type { ObsidiousVaultItem, ObsidiousVaultData, ObsidiousFileTreeNode } from "./ObsidiousVault";
+
+export { ObsidiousVault, RemarkObsidious, hash, slugify, slugifyFilepath, ObsidiousVaultImageFiletypes };
